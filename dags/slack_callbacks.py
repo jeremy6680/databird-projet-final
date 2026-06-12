199378ba6bbcc4493337ae50930a7eb71d25a86f
Jeremy Marchandeau <jerem9911@hotmail.com>
2026-06-12 11:55:08 +0200

    Refactor Slack failure alert to use requests and update requirements to remove Slack provider

--- dags/slack_callbacks.py
+++ dags/slack_callbacks.py
@@ -1,20 +1,23 @@
 import os
-from airflow.providers.slack.hooks.slack_webhook import SlackWebhookHook
+import requests
 
 
 def on_failure_slack_alert(context):
+    webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
+    if not webhook_url:
+        return
+
     dag_id = context["dag"].dag_id
     task_id = context["task_instance"].task_id
-    exec_date = context["execution_date"]
+    logical_date = context["logical_date"]
     log_url = context["task_instance"].log_url
 
     message = (
         f":red_circle: *Échec dans le pipeline dbt*\n"
         f"*DAG:* `{dag_id}`\n"
         f"*Tâche:* `{task_id}`\n"
-        f"*Date d'exécution:* {exec_date}\n"
+        f"*Date d'exécution:* {logical_date}\n"
         f"*Logs:* <{log_url}|Voir les logs>"
     )
 
-    hook = SlackWebhookHook(slack_webhook_conn_id="slack_webhook")
-    hook.send(text=message)
+    requests.post(webhook_url, json={"text": message}, timeout=10)
